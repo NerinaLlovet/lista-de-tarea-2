ca33bf5df7661eb7a567ad32f4b79c49614dfb5b
NerinaLlovet <nerinagllovet@gmail.com>
2025-03-26 17:09:09 -0300

    js

---
+++ main.js
@@ -0,0 +1,26 @@
+const input = document.getElementById("input-agregar-tarea");
+console.log(input);
+const mas = document.getElementById ("mas-agregar-tarea");
+const lista = document.getElementById ("lista");
+const agregarTarea = (tarea) => {
+ const elemento = `
+ <li class="item" id="item">
+          <i class="fas fa-circle co check" id="check" data-="check"></i> 
+          <p class="texto">${tarea}</p>
+          <i class="fas fa-trash de borrar" id="borrar" data-="borrar"></i> 
+        </li>
+ 
+        `
+lista.insertAdjacentHTML("beforeend", elemento); 
+};
+
+
+//llamas funcion 
+mas.addEventListener(`click`, () => {
+const tarea = input.value 
+if(tarea) {
+    agregarTarea(tarea)
+}
+input.value = "";
+
+})
