9a14991dbf2ed9407a0e4e578cfe41bd364c8ece
NerinaLlovet <nerinagllovet@gmail.com>
2025-04-16 16:57:24 -0300

    error

--- main.js
+++ main.js
@@ -2,18 +2,45 @@ const input = document.getElementById("input-agregar-tarea");
 console.log(input);
 const mas = document.getElementById ("mas-agregar-tarea");
 const lista = document.getElementById ("lista");
-const agregarTarea = (tarea) => {
+
+const hecho = 'fa-check-circle';
+const pendiente = 'fa-circle';
+const tachado = 'tachado';
+
+const agregarTarea = (tarea,check) => {
+  const estado = check ? hecho : pendiente ;
+  const tachar = check ? tachado : "" ;
+
+
  const elemento = `
  <li class="item" id="item">
-          <i class="fas fa-circle co check" id="check" data-="check"></i> 
-          <p class="texto">${tarea}</p>
-          <i class="fas fa-trash de borrar" id="borrar" data-="borrar"></i> 
+          <i class="far ${estado} check " id="check" data="check"></i> 
+          <p class="texto ${tachar}">${tarea}</p>
+          <i class="fas fa-trash de borrar" id="borrar" data="borrar"></i> 
         </li>
  
         `
 lista.insertAdjacentHTML("beforeend", elemento); 
 };
 
+
+const tareaRealizada = (element) => {
+  element.classList.toggle(hecho);
+  element.clasList.toggle(pendiente);
+  element.parentNode.querySelector('.tarea').classList.toggle(tachado);
+};
+
+
+const cambiarEstilos = () => {
+  const link =document.getElementById("estilos");
+
+  link.href = link.href.includes("style.css") ? "style2.css" : "style.css" ;
+} ;
+
+
+const tareaEliminada = (element) => {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+};
 
 //llamas funcion 
 mas.addEventListener(`click`, () => {
@@ -36,9 +63,14 @@ input.value = "";
   }
 })
 
-const cambiarEstilos = () => {
-  const link =document.getElementById("estilos");
 
-  link.href = link.href.includes("style.css") ? "style2.css" : "style.css" ;
-} ;
-
+lista.addEventListener('click',function(event){
+  const element = event.target;
+  const elementData  =element.atributes.data.value;
+  if (elementData == 'check'){
+    tareaRealizada(element)
+  }
+  else if (elementData == 'borrar'){
+    tareaEliminada(element)
+  }
+});
